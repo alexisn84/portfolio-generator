c0422748485a0f764072e4c2a46c2f7466de7624
alexisn84 <91172382+alexisn84@users.noreply.github.com>
2021-11-26 14:51:54 -0500

    create html thru node and js

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
 
 
-// Notice the lack of parentheses around the `profileDataArr` parameter?
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
-printProfileData(profileDataArgs);
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
